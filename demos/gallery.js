ebd685acc9213240b73b76abacbc642545d7a3ea
XHiddenProjects <64079569+XHiddenProjects@users.noreply.github.com>
2025-05-10 20:50:07 -0400

    Update gallery.js

--- demos/gallery.js
+++ demos/gallery.js
@@ -1,6 +1,6 @@
-import { startup, Gallery } from "../js/mediaviewer.js";
+import { startup, Gallery } from "../js/es6/mediaviewer.js";
 startup();
-new Gallery('.gallery',{
+new Gallery('.demo',{
     images: ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg'],
     captions: ['Rome', 'Jaguar', 'Boat'],
     zoom: true,
