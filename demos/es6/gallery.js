047b8c1060012699d4be23d420f0952ef928b7dd
Gavin Zeager <64079569+XHiddenProjects@users.noreply.github.com>
2025-05-26 07:56:28 -0400

    es6
    support only es6

--- demos/es6/gallery.js
+++
@@ -1,9 +0,0 @@
-import { startup, Gallery } from "../js/es6/mediaviewer.js";
-startup();
-new Gallery('.demo',{
-    images: ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg'],
-    captions: ['Rome', 'Jaguar', 'Boat'],
-    zoom: true,
-    autoResize: true,
-    static: false
-});
