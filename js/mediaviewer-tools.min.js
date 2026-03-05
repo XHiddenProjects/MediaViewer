93fecead353bbe092cc13f2b46813d2162693c41
XHiddenProjects <64079569+XHiddenProjects@users.noreply.github.com>
2026-03-05 17:59:15 -0500

    Add files via upload

--- js/mediaviewer-tools.min.js
+++ js/mediaviewer-tools.min.js
@@ -1,6 +1,6 @@
 /**
  * @package MediaViewer
- * @version 1.2.6
+ * @version 1.3.0
  * @description A javascript library to create a media viewing experience
  * @license MIT
  * @author XHiddenProjects
